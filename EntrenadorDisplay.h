db8afcddf20850dc95f3ea5c2d9b1146fbbd7a1f
kevinrosadigitalcodesign <kevin.rosa@digitalcodesign.com>
2023-09-07 10:48:07 +0100

    Created display 7 segments module library

---
+++ EntrenadorDisplay.h
@@ -0,0 +1,30 @@
+/*
+ * EntrenadorDisplay.h - Librería del módulo de Display para Entrenador Arduino
+ *
+ * Creado el 07/09/2023 por Kevin Rosa - kevin.ingenieria@digitalcodesign.com
+ * 
+ * Declaración de atributos y métodos para la clase EntrenadorDisplay,
+ * referida al Entrenador compatible con Arduino y el módulo de display 7 segmentos.
+ * 
+ * -> DHT
+ * -> MQ
+ * -> HC-SR04
+ * -> DS18B20
+ * -> LDR
+ */
+
+#ifndef EntrenadorDisplay_h
+#define EntrenadorDisplay_h
+
+    #include "Arduino.h"
+
+    class EntrenadorDisplay
+    {
+
+        public:
+
+            EntrenadorDisplay();
+
+        private:
+
+    }
