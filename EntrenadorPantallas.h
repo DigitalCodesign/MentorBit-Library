d1ca036ca608e2489beaa3d0c2faf88d9f265220
kevinrosadigitalcodesign <kevin.rosa@digitalcodesign.com>
2023-09-07 10:46:51 +0100

    Created Screens module library

---
+++ EntrenadorPantallas.h
@@ -0,0 +1,25 @@
+/*
+ * EntrenadorPantallas.h - Librería del módulo de Pantallas para Entrenador Arduino
+ *
+ * Creado el 07/09/2023 por Kevin Rosa - kevin.ingenieria@digitalcodesign.com
+ * 
+ * Declaración de atributos y métodos para la clase EntrenadorPantallas,
+ * referida al Entrenador compatible con Arduino y el módulo de pantallas.
+ * 
+ */
+
+#ifndef EntrenadorPantallas_h
+#define EntrenadorPantallas_h
+
+    #include "Arduino.h"
+
+    class EntrenadorPantallas
+    {
+
+        public:
+
+            EntrenadorPantallas();
+
+        private:
+
+    }
