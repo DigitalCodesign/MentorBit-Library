572027a8a89f885bf5258ab29a6e96c09ce140fe
kevinrosadigitalcodesign <kevin.rosa@digitalcodesign.com>
2023-09-12 16:53:36 +0100

    Created examples for MQ, DHT and DS18B20

---
+++ examples/SensoresDHT/SensoresDHT.ino
@@ -0,0 +1,39 @@
+/**
+ * SensoresDHT.ino - Ejemplo de uso de DHT del Entrenador
+ * 
+ * Creado el 12/09/2023 por Kevin Rosa - kevin.ingenieria@digitalcodesign.com
+ * 
+ * Volcado de información del sensor al monitor serial.
+ */
+
+#include "Entrenador.h"
+#include "EntrenadorSensores.h"
+
+// Declaración de instancia de las clases
+Entrenador miEntrenador;
+EntrenadorSensores miEntrenadorSensores;
+
+// Inicialización del sketch
+void setup() {
+
+    Serial.begin(9600);
+
+}
+
+// Bucle principal del sketch
+void loop() {
+
+    // Obtención de las lecturas del sensor
+    float temperatura = miEntrenadorSensores.obtenerTemperaturaDHT();
+    float humedad = miEntrenadorSensores.obtenerHumedadDHT();
+
+    // Mostrar datos por monitor serial
+    Serial.println("--------------------");
+    Serial.print("Lectura de temperatura: ");
+    Serial.println(temperatura);
+    Serial.print("Lectura de humedad: ");
+    Serial.println(humedad);
+
+    delay(500);
+
+}
--- examples/SensoresMQ/SensoresMQ.ino
+++ examples/SensoresMQ/SensoresMQ.ino
@@ -1,5 +1,5 @@
 /**
- * SensoresLDR.ino - Ejemplo de uso de MQ del Entrenador
+ * SensoresMQ.ino - Ejemplo de uso de MQ del Entrenador
  * 
  * Creado el 12/09/2023 por Kevin Rosa - kevin.ingenieria@digitalcodesign.com
  * 
