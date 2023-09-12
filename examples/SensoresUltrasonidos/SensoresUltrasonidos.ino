7c5ba00eeee55bb0aee1b7ec59d7efeb6c00d5c4
kevinrosadigitalcodesign <kevin.rosa@digitalcodesign.com>
2023-09-12 16:56:57 +0100

    Created example for HC-SR04, still need to test it

---
+++ examples/SensoresUltrasonidos/SensoresUltrasonidos.ino
@@ -0,0 +1,37 @@
+/**
+ * SensoresUltrasonidos.ino - Ejemplo de uso de Ultrasonidos del Entrenador
+ * 
+ * Creado el 12/09/2023 por Kevin Rosa - kevin.ingenieria@digitalcodesign.com
+ * 
+ * Volcado de información del sensor al monitor serial.
+ */
+
+#include "Entrenador.h"
+#include "EntrenadorSensores.h"
+
+// Declaración de instancia de las clases
+Entrenador miEntrenador;
+EntrenadorSensores miEntrenadorSensores;
+
+// Inicialización del sketch
+void setup() {
+
+    Serial.begin(9600);
+
+}
+
+// Bucle principal del sketch
+void loop() {
+
+    // Obtención de la lectura del sensor
+    float distancia = miEntrenadorSensores.obtenerDistanciaUlrasonidos();
+
+    // Mostrar datos por monitor serial
+    Serial.println("--------------------");
+    Serial.print("Distancia: ");
+    Serial.print(distancia);
+    Serial.println(" cm");
+
+    delay(500);
+
+}
