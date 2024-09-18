df45186388bf680e6b06a26f926610f3f50bf1f4
kevinrosadigitalcodesign <kevin.rosa@digitalcodesign.com>
2024-09-18 09:13:04 +0100

    Fixed Generic Ports module waiting to new implementation with automatic assignation functionality

--- MentorBitPuertos.cpp
+++ MentorBitPuertos.cpp
@@ -19,34 +19,4 @@
  * -> Puerto genérico 2
  * -> Puerto genérico 3
  */
-MentorBitPuertos::MentorBitPuertos()
-{
-
-    puerto1_digital1 = 35;
-    puerto1_digital2 = 36;
-    puerto1_digital3 = 37;
-    puerto1_digital4 = 38;
-    puerto1_digital5 = 11;
-    puerto1_digital6 = 12;
-    puerto1_analog1 = PIN_A10;
-    puerto1_analog2 = PIN_A11;
-
-    puerto2_digital1 = 39;
-    puerto2_digital2 = 40;
-    puerto2_digital3 = 41;
-    puerto2_digital4 = 42;
-    puerto2_digital5 = 13;
-    puerto2_digital6 = 44;
-    puerto2_analog1 = PIN_A12;
-    puerto2_analog2 = PIN_A13;
-
-    puerto3_digital1 = 43;
-    puerto3_digital2 = 47;
-    puerto3_digital3 = 48;
-    puerto3_digital4 = 49;
-    puerto3_digital5 = 45;
-    puerto3_digital6 = 46;
-    puerto3_analog1 = PIN_A14;
-    puerto3_analog2 = PIN_A15;
-
-}
+MentorBitPuertos::MentorBitPuertos() {}
--- MentorBitPuertos.h
+++ MentorBitPuertos.h
@@ -16,39 +16,39 @@
 
     #include "Arduino.h"
 
+    #define PUERTO_1_DIGITAL_1 35
+    #define PUERTO_1_DIGITAL_2 36
+    #define PUERTO_1_DIGITAL_3 37
+    #define PUERTO_1_DIGITAL_4 38
+    #define PUERTO_1_DIGITAL_5 3
+    #define PUERTO_1_DIGITAL_6 2
+    #define PUERTO_1_ANALOG_1 PIN_A10
+    #define PUERTO_1_ANALOG_2 PIN_A11
+
+    #define PUERTO_2_DIGITAL_1 39
+    #define PUERTO_2_DIGITAL_2 40
+    #define PUERTO_2_DIGITAL_3 41
+    #define PUERTO_2_DIGITAL_4 42
+    #define PUERTO_2_DIGITAL_5 13
+    #define PUERTO_2_DIGITAL_6 44
+    #define PUERTO_2_ANALOG_1 PIN_A12
+    #define PUERTO_2_ANALOG_2 PIN_A13
+
+    #define PUERTO_3_DIGITAL_1 43
+    #define PUERTO_3_DIGITAL_2 47
+    #define PUERTO_3_DIGITAL_3 48
+    #define PUERTO_3_DIGITAL_4 49
+    #define PUERTO_3_DIGITAL_5 45
+    #define PUERTO_3_DIGITAL_6 46
+    #define PUERTO_3_ANALOG_1 PIN_A14
+    #define PUERTO_3_ANALOG_2 PIN_A15
+
     class MentorBitPuertos
     {
 
         public:
 
             MentorBitPuertos();
-
-            uint8_t puerto1_digital1;
-            uint8_t puerto1_digital2;
-            uint8_t puerto1_digital3;
-            uint8_t puerto1_digital4;
-            uint8_t puerto1_digital5;
-            uint8_t puerto1_digital6;
-            uint8_t puerto1_analog1;
-            uint8_t puerto1_analog2;
-
-            uint8_t puerto2_digital1;
-            uint8_t puerto2_digital2;
-            uint8_t puerto2_digital3;
-            uint8_t puerto2_digital4;
-            uint8_t puerto2_digital5;
-            uint8_t puerto2_digital6;
-            uint8_t puerto2_analog1;
-            uint8_t puerto2_analog2;
-
-            uint8_t puerto3_digital1;
-            uint8_t puerto3_digital2;
-            uint8_t puerto3_digital3;
-            uint8_t puerto3_digital4;
-            uint8_t puerto3_digital5;
-            uint8_t puerto3_digital6;
-            uint8_t puerto3_analog1;
-            uint8_t puerto3_analog2;
 
     };
 
