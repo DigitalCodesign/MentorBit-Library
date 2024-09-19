05dc225a082d385f2b7716f32fdde1a446b0613f
kevinrosadigitalcodesign <kevin.rosa@digitalcodesign.com>
2024-09-19 15:25:51 +0100

    Update the example

--- examples/Sensores/LDR/LDR.ino
+++ examples/Sensores/LDR/LDR.ino
@@ -17,7 +17,7 @@ MentorBitSensores miMentorBitSensores;
 // Inicialización del sketch
 void setup() {
 
-    digitalWrite(miMentorBit.ledAzul, LOW);
+    digitalWrite(miMentorBit.LED_ROJO, LOW);
 
 }
 
@@ -25,11 +25,11 @@ void setup() {
 void loop() {
 
     // Obtención y conversión de la lectura del sensor
-    int lectura0a1023 = analogRead(miMentorBitSensores.ldr);
+    int lectura0a1023 = miMentorBitSensores.obtenerLecturaLDR();
     float lectura0a255 = (lectura0a1023 / 1023.00) * 255;
 
     // Modificación de la intensidad del LED
-    analogWrite(miMentorBit.ledAzul, lectura0a255);
+    analogWrite(miMentorBit.LED_ROJO, lectura0a255);
     delay(100);
 
 }
