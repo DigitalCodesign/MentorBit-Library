7bb77c6b99c96e3639c3a7936ed34a5f5f036127
krosDC <kevin.ingenieria@digitalcodesign.com>
2026-01-29 13:48:23 +0000

    Implemented the RTC DS1307 control in the library and removed RTClib usage. Also improved the DataLogger module with some methods.

--- MentorBitDataLogger.cpp
+++ MentorBitDataLogger.cpp
@@ -16,72 +16,9 @@
  * Devuelve una instancia de la clase MentorBitDataLogger que provee los
  * pines y métodos necesarios para controlar la tarjeta SD y el RTC en el MentorBit.
  */
-MentorBitDataLogger::MentorBitDataLogger() {}
+MentorBitDataLogger::MentorBitDataLogger() : _rtc_address(0x68) {}
 
-/*
- * Función para inicializar el RTC.
- * 
- * Devuelve un booleano, true si consigue inicializar correctamente
- * y false si no es capaz de hacerlo.
- */
-boolean MentorBitDataLogger::inicializarRTC()
-{
-
-    if(!MentorBitDataLogger::rtc.begin())
-    {
-
-        return false;
-
-    }
-
-    return true;
-
-}
-
-/*
- * Función para ajustar la hora del RTC a la hora dada.
- * 
- * No devuelve ningún valor.
- */
-void MentorBitDataLogger::ajustarHoraRTC(
-    uint16_t anyo,
-    uint8_t mes,
-    uint8_t dia,
-    uint8_t hora=0,
-    uint8_t minuto=0,
-    uint8_t segundo=0
-)
-{
-
-    MentorBitDataLogger::rtc.adjust(DateTime(anyo, mes, dia, hora, minuto, segundo));
-
-}
-
-/*
- * Función para ajustar la hora del RTC por defecto a la hora del sistema.
- * 
- * No devuelve ningún valor.
- */
-void MentorBitDataLogger::ajustarHoraRTC()
-{
-
-    MentorBitDataLogger::rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-
-}
-
-/*
- * Función que devuelve la hora actual del RTC.
- * 
- * Hora actual en formato cadena de texto.
- */
-String MentorBitDataLogger::obtenerHoraRTC()
-{
-
-    DateTime actualTime = MentorBitDataLogger::rtc.now();
-
-    return String(actualTime.timestamp(DateTime::TIMESTAMP_FULL));
-
-}
+// --- Métodos específicos de la Tarjeta SD
 
 /*
  * Función para inicializar la tarjeta SD.
@@ -93,11 +30,7 @@ boolean MentorBitDataLogger::inicializarSD()
 {
 
     if(!SD.begin(SD_CHIP_SELECT))
-    {
-
         return false;
-
-    }
 
     return true;
 
@@ -112,19 +45,16 @@ boolean MentorBitDataLogger::inicializarSD()
 boolean MentorBitDataLogger::escribirEnArchivo(String nombre, String mensaje)
 {
 
-    MentorBitDataLogger::_dataFile = SD.open(nombre, FILE_WRITE);
+    _data_file = SD.open(nombre, FILE_WRITE);
 
-    if(MentorBitDataLogger::_dataFile)
+    if(_data_file)
     {
-
-        MentorBitDataLogger::_dataFile.println(mensaje);
-        MentorBitDataLogger::_dataFile.close();
-
+        _data_file.println(mensaje);
+        _data_file.close();
         return true;
-
+    } else {
+        return false;
     }
-
-    return false;
 
 }
 
@@ -137,26 +67,237 @@ boolean MentorBitDataLogger::escribirEnArchivo(String nombre, String mensaje)
 String MentorBitDataLogger::leerDeArchivo(String nombre)
 {
 
-    String dataFromFile = "None";
-    MentorBitDataLogger::_dataFile = SD.open(nombre, FILE_READ);
+    String contenido = "";
+    _data_file = SD.open(nombre, FILE_READ);
 
-    if(MentorBitDataLogger::_dataFile)
+    if(_data_file)
     {
+        while(_data_file.available())
+            contenido += (char)_data_file.read();
 
-        char incomingChar = NULL;
-        dataFromFile = "";
-        while(MentorBitDataLogger::_dataFile.available())
-        {
-
-            incomingChar = MentorBitDataLogger::_dataFile.read();
-            dataFromFile += incomingChar;
-
-        }
-
-        MentorBitDataLogger::_dataFile.close();
+        _data_file.close();
 
     }
 
-    return dataFromFile;
+    return contenido;
+
+}
+
+/*
+ * Función para combinar la lectura del RTC con la escritura en la SD para guardar un LOG.
+ * 
+ * Devuelve un booleano, true si consigue guardar correctamente
+ * y false si no es capaz de hacerlo.
+ */
+boolean MentorBitDataLogger::escribirLog(String nombre_archivo, String mensaje)
+{
+
+    String time_stamp = obtenerHoraRTC("[DD/MM/YYYY hh:mm:ss]");
+    String log = time_stamp + " " + mensaje;
+    return escribirEnArchivo(nombre_archivo, log);
+
+}
+
+// --- Métodos específicos del Reloj en Tiempo Real
+
+/*
+ * Función para inicializar el RTC.
+ * 
+ * Devuelve un booleano, true si consigue inicializar correctamente
+ * y false si no es capaz de hacerlo.
+ */
+boolean MentorBitDataLogger::inicializarRTC()
+{
+
+    Wire.begin();
+    Wire.beginTransmission(_rtc_address);
+    if (Wire.endTransmission() != 0) return false;
+
+    if (!rtcFuncionando())
+    {
+        Wire.beginTransmission(_rtc_address);
+        Wire.write(0x00);
+        Wire.write(0x00);
+        Wire.endTransmission();
+    }
+
+    return true;
+
+}
+
+/*
+ * Función para ajustar la hora del RTC a la hora dada.
+ * 
+ * No devuelve ningún valor.
+ */
+void MentorBitDataLogger::ajustarHoraRTC(uint16_t anyo, uint8_t mes, uint8_t dia, uint8_t hora, uint8_t minuto, uint8_t segundo)
+{
+
+    Wire.beginTransmission(_rtc_address);
+    Wire.write(0x00);
+    Wire.write(decToBcd(segundo));
+    Wire.write(decToBcd(minuto));
+    Wire.write(decToBcd(hora));
+    Wire.write(decToBcd(0));
+    Wire.write(decToBcd(dia));
+    Wire.write(decToBcd(mes));
+    Wire.write(decToBcd(anyo - 2000));
+    Wire.endTransmission();
+
+}
+
+/*
+ * Función para ajustar la hora del RTC por defecto a la hora del sistema.
+ * 
+ * No devuelve ningún valor.
+ */
+void MentorBitDataLogger::ajustarHoraRTC()
+{
+
+    const char* months = "JanFebMarAprMayJunJulAugSepOctNovDec";
+    char temp_month[4];
+
+    temp_month[0] = __DATE__[0];
+    temp_month[1] = __DATE__[1];
+    temp_month[2] = __DATE__[2];
+    temp_month[3] = 0;
+
+    uint8_t mes = 0;
+    for (int i = 0 ; i < 12 ; i++)
+    {
+        if (strncmp(months + (i * 3), temp_month, 3) == 0)
+        {
+            mes = i + 1;
+            break;
+        }
+    }
+
+    uint8_t dia = atoi(__DATE__ + 4);
+    uint8_t anyo = atoi(__DATE__ + 7);
+    uint8_t hora = atoi(__TIME__);
+    uint8_t minuto = atoi(__TIME__ + 3);
+    uint8_t segundo = atoi(__TIME__ + 6);
+
+    ajustarHoraRTC(anyo, mes, dia, hora, minuto, segundo);
+
+}
+
+/*
+ * Función que devuelve la hora actual del RTC.
+ * 
+ * Hora actual en formato cadena de texto.
+ */
+String MentorBitDataLogger::obtenerHoraRTC(String formato)
+{
+
+    _leerRTC();
+
+    String s_hh = (_hh < 10 ? "0" : "") + String(_hh);
+    String s_mm = (_mm < 10 ? "0" : "") + String(_mm);
+    String s_ss = (_ss < 10 ? "0" : "") + String(_ss);
+    String s_DD = (_d < 10 ? "0" : "") + String(_d);
+    String s_MM = (_m < 10 ? "0" : "") + String(_m);
+    String s_YYYY = String(_y);
+
+    formato.replace("hh", s_hh);
+    formato.replace("mm", s_mm);
+    formato.replace("ss", s_ss);
+    formato.replace("DD", s_DD);
+    formato.replace("MM", s_MM);
+    formato.replace("YYYY", s_YYYY);
+
+    return formato;
+
+}
+
+/**
+ * Función para comprobar si el reloj está caminando.
+ * 
+ * Devuelve true si está funcionando, false en caso contrario.
+ */
+boolean MentorBitDataLogger::rtcFuncionando()
+{
+
+    Wire.beginTransmission(_rtc_address);
+    Wire.write(0x00);
+    Wire.endTransmission();
+
+    Wire.requestFrom(_rtc_address, 1);
+    uint8_t ss = Wire.read();
+
+    return !(ss >> 7);
+
+}
+
+// --- Métodos extra (NVRAM)
+
+/*
+ * Función para almacenar un valor en la memoria interna del RTC.
+ * 
+ * No devuelve ningún valor.
+ */
+void MentorBitDataLogger::guardarDato(uint8_t direccion, uint8_t dato)
+{
+
+    if (direccion > 55) return;
+
+    Wire.beginTransmission(_rtc_address);
+    Wire.write(0x08 + direccion);
+    Wire.write(dato);
+    Wire.endTransmission();
+
+}
+
+/**
+ * Función para recuperar un valor de la memoria interna del RTC.
+ * 
+ * Devuelve el dato recuperado.
+ */
+uint8_t MentorBitDataLogger::leerDato(uint8_t direccion)
+{
+
+    if (direccion > 55) return;
+
+    Wire.beginTransmission(_rtc_address);
+    Wire.write(0x08 + direccion);
+    Wire.endTransmission();
+
+    Wire.requestFrom(_rtc_address, 1);
+    return Wire.read();
+
+}
+
+// --- Métodos privados
+
+uint8_t MentorBitDataLogger::bcdToDec(uint8_t valor)
+{
+
+    return ( (valor / 16 * 10) + (valor % 16) );
+
+}
+
+uint8_t MentorBitDataLogger::decToBcd(uint8_t valor)
+{
+
+    return ( (valor / 10 * 16) + (valor % 10) );
+
+}
+
+void MentorBitDataLogger::_leerRTC()
+{
+
+    Wire.beginTransmission(_rtc_address);
+    Wire.write(0x00);
+    Wire.endTransmission();
+
+    Wire.requestFrom(_rtc_address, 7);
+
+    _ss = bcdToDec(Wire.read() & 0x7F);
+    _mm = bcdToDec(Wire.read());
+    _hh = bcdToDec(Wire.read() & 0x3F);
+    Wire.read();
+    _d = bcdToDec(Wire.read());
+    _m = bcdToDec(Wire.read());
+    _y = bcdToDec(Wire.read()) + 2000;
 
 }
